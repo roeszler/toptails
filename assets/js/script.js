725b7f1acbfb39fecda6800bfa2da7357a4e070c
Stuart Roeszler <stuart@roeszler.com>
2022-04-21 17:39:28 +0000

    FEAT: code winner alert modal render and vanish to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,23 @@
 
 // ------ Functions
 
+// Modal Displays to indicate user initiated events
+
+/**
+* ------ Display winnerPopup section & hide game-area section
+*/
+function winnerRender() {
+   document.getElementById('winnerPopup').style.display = 'block';
+   document.getElementById('game-area').style.display = 'none';
+}
+/**
+* Display game-area section & hide winnerPopup section
+*/
+function winnerOk() {
+   document.getElementById('winnerPopup').style.display = 'none';
+   document.getElementById('game-area').style.display = 'block';
+}
+
 // ------ DOM Listeners
 // Window: DOMContentLoaded events
 
