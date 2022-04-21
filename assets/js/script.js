131ce5470f3ad53dd7f56bfe9aa0480841bcd464
Stuart Roeszler <stuart@roeszler.com>
2022-04-21 17:42:25 +0000

    STYLE: add global styling to alert modals in style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,27 @@
 // Modal Displays to indicate user initiated events
 
 /**
-* ------ Display winnerPopup section & hide game-area section
+* Display winnerPopup section & hide game-area section
 */
 function winnerRender() {
    document.getElementById('winnerPopup').style.display = 'block';
    document.getElementById('game-area').style.display = 'none';
+   document.getElementById('gameplayPopup').style.display = 'none';
+   document.getElementById('hhSpin').style.display = 'none';
+   document.getElementById('htSpin').style.display = 'none';
+   document.getElementById('ttSpin').style.display = 'none';
 }
+
 /**
 * Display game-area section & hide winnerPopup section
 */
 function winnerOk() {
    document.getElementById('winnerPopup').style.display = 'none';
    document.getElementById('game-area').style.display = 'block';
+   document.getElementById('gameplayPopup').style.display = 'none';
 }
+
+
 
 // ------ DOM Listeners
 // Window: DOMContentLoaded events
